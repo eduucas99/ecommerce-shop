5843fc8054701cd226bce05b8343b2e825feecfc
eduucas99 <eduardocas99@hotmail.com>
2026-09-21 12:21:54 -0300

    add directory

---
+++ src/app/(shop)/page.tsx
@@ -0,0 +1,12 @@
+import { titleFont } from "@/config/fonts";
+
+export default function Home() {
+  return (
+    <main className="">
+      <h1>
+        Hola Mundo
+      </h1>
+      <h1 className={`${titleFont.className} font-bold`}>Hola Mundo</h1>
+    </main>
+  );
+}
