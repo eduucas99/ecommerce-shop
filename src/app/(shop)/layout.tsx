9c0fb26f292e29b818036b69d5fe0624ec6c6f0b
eduucas99 <eduardocas99@hotmail.com>
2026-09-21 17:48:31 -0300

    add Sidebar

--- src/app/(shop)/layout.tsx
+++ src/app/(shop)/layout.tsx
@@ -1,4 +1,4 @@
-import { TopMenu } from "@/components";
+import { Sidebar, TopMenu } from "@/components";
 export default function ShopLayout({
  children
 }: {
@@ -6,7 +6,9 @@ export default function ShopLayout({
 }) {
   return (
     <main className="min-h-screen">
-        <TopMenu />
+      <TopMenu />
+      <Sidebar />
+      
       <div className="px-0 sm:px-10">
         {children}
       </div>
