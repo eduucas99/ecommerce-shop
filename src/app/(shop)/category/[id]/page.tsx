11a821e45daf8d3e002722bf8a9f8793137be781
eduucas99 <eduardocas99@hotmail.com>
2026-09-21 13:34:34 -0300

    add notfound

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,8 +1,21 @@
+import { notFound } from "next/navigation";
 
-export default function() {
+interface Props {
+  params: {
+    id: string;
+  },
+}
+
+export default async function({params}: Props) {
+  const { id } = await params;
+
+  if(id === 'kids'){
+    notFound();
+  }
+
   return (
     <div>
-      <h1>Category Page</h1>
+      <h1>Category Page { id }</h1>
     </div>
   );
 }
