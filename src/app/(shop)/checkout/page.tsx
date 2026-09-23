50ea3a38cad9b51dbcb2df0c8c30958bea3d0526
eduucas99 <eduardocas99@hotmail.com>
2026-09-23 11:58:03 -0300

    add checkoutPage

--- src/app/(shop)/checkout/page.tsx
+++ src/app/(shop)/checkout/page.tsx
@@ -1,8 +1,99 @@
+import { Title } from "@/components";
+import { initialData } from "@/seed/seed";
+import Link from "next/link";
+import Image from "next/image";
+import { QuantitySelector } from "@/components";
+
+const productsInCart = [
+  initialData.products[0],
+  initialData.products[1],
+  initialData.products[2],
+];
 
 export default function() {
   return (
-    <div>
-      <h1>Checkout Page</h1>
+    <div className="flex justify-center items-center mb-72 px-10 sm:px-0">
+      
+      <div className="flex flex-col w-250">
+        <Title title="Verificar Orden"/>
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-10">
+          {/* Carrito */}
+          <div className="flex flex-col mt-5">
+            <span className="text-xl">Ajustar elementos</span>
+            <Link href='/cart' className="underline mb-5">
+              Editar carrito
+            </Link>
+          
+            {/* Items */}
+            {
+              productsInCart.map(product => (
+                <div key={product.slug} className="flex mb-5">
+                  <Image
+                    src={`/products/${product.images[0]}`}
+                    width={100}
+                    height={100}
+                    style={{
+                      width: '100px',
+                      height: '110px',
+                    }}
+                    alt={product.title}
+                    className="mr-5 rounded"
+                  />
+                  <div className="mt-4">
+                    <p>{product.title}</p>
+                    <p>$ {product.price} x 3</p>
+                    <p className="font-bold">Subtotal: ${product.price * 3}</p>
+                  </div>
+                </div>
+              ))
+            }
+          </div>
+
+          {/* Checkout - Resumen de la compra */}
+          <div className="bg-white rounded-xl shadow-xl p-7">
+            <h2 className="text-2xl font-bold mb-2">Dirección de entrega</h2>
+            <div className="mb-10">
+              <p className="text-xl">Eduardo Castañeira</p>
+              <p>Av. Aristobulo del Valle</p>
+              <p>Santa Fe</p>
+              <p>Santa Fe</p>
+              <p>CP: 3004</p>
+            </div>
+
+            {/* Divider */}
+            <div className="w-full h-0.5 rounded bg-gray-200 mb-5" />
+            <h2 className="text-2xl mb-2">Resumen de compra</h2>
+            
+            <div className="grid grid-cols-2">
+              <span>N° Productos</span>
+              <span className="text-right">3 Artículos</span>
+              
+              <span>Subtotal</span>
+              <span className="text-right">$ 100.000</span>
+              
+              <span>Impuestos (%15)</span>
+              <span className="text-right">$ 18.000</span>
+              
+              <span className="text-2xl mt-5">Total</span>
+              <span className="text-2xl mt-5 text-right">$ 118.000</span>
+            </div>
+
+            <div className="mt-5 mb-2 w-full">
+              <p className="mb-5">
+                {/* Disclaimer */}
+                <span className="text-xs">
+                  Al hacer clic en "Colocar orden", aceptas nuestros <a href="#" className="underline">términos y condiciones</a> y <a href="#" className="underline">política de privacidad.</a>
+                </span>
+              </p>
+              <Link
+                className="flex btn-primary justify-center" 
+                href="/orders/123">
+                Colocar Orden
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
